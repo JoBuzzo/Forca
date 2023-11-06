158bf80aeeaf190f3e3c214fceae37d833f7492e
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2023-11-06 10:31:16 -0300

    New application name

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center">
     <img src="{{ asset('images/forca.png') }}" alt="LOGO" class="w-14">
     <div class="flex flex-col items-start">
-        <h1 class="text-base text-[#4000FF] font-extrabold">GALLOWS<span class="text-[#02E65A]">LEAGUE</span></h1>
+        <h1 class="text-base text-[#4000FF] font-extrabold">HANGMAN<span class="text-[#02E65A]">LEAGUE</span></h1>
     </div>
 </div>
